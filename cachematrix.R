99a5a5fbf4fa9de3eab1b849b9b6950d52a3d8c8
danielebarro <43915546+danielebarro@users.noreply.github.com>
2020-01-06 14:54:00 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,21 +4,21 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix = function( x = matrix() ) {
-    i = NULL #Initialize the solution to the inverse of the matrix to be a NULL value
+    s = NULL #Initialize the solution to the inverse of the matrix to be a NULL value
       
     set = function(y){ #Defines a function with name 'set' 
       x <<- y #Set the Matrix, x, to a new Matrix, y,
-      i <<- NULL #reset the value of the inverse of the matrix equal to NULL
+      s <<- NULL #reset the value of the inverse of the matrix equal to NULL
     }
         
     get = function() x #Assigne to 'get' a function that returns the Matrix 'x'
-    setinverse = function(inverse) i <<- inverse #Assigne to 'setinverse' a function that set the inverse, i, to inverse
-    getinverse = function() i #Return the inverse, i, of the matrix. If the inverse function has 
+    setinverse = function(inverse) s <<- inverse #Assigne to 'setinverse' a function that set the inverse, s, to inverse
+    getinverse = function() s #Return the inverse, s, of the matrix. If the inverse function has 
                               #been manually setted than it returnes the setted inverse matrix
     
    #Returns a special vector containing the functions just defined
    list(set = set, #gives the name 'set' to the function set() defined above
-         get= get, #gives the name 'get' to the function get() defined above
+         get = get, #gives the name 'get' to the function get() defined above
          setinverse=setinverse, #gives the name 'setinverse' to the function setinverse() defined above
          getinverse=getinverse) #gives the name 'getinverse' to the function getinverse() defined above
 }
@@ -26,13 +26,13 @@ makeCacheMatrix = function( x = matrix() ) {
 
  ## Return a matrix that is the inverse of 'x'
 cacheSolve <- function(x, ...) {
-          i=x$getinverse() #Assigne to, i, the solution of the stored inverted matrix
-                if (!is.null(i)){ #Condition to check if the any inverted matrix has been stored 
+          s = x$getinverse() #Assigne to, is the solution of the stored inverted matrix
+                if (!is.null(s)){ #Condition to check if the any inverted matrix has been stored 
                          message("getting cached data") #Worning message that displayes only if the inverse matrix has been stored
-                         return(i) #Returns the stored matrix only if it is not NULL
+                         return(s) #Returns the stored matrix only if it is not NULL
                         }
-          data=x$get() #Assigne to 'data' the the function 'get' so that the variable 'data' now containes the matrix give by the user
-          i=solve(data,...) #Assigne to 'i' the solution to the inverse of the matrix given in input
-          x$setinverse(i) #Call the function setinverse and store the invertse matrix
-          i #Print the inverted matrix
+          data = x$get() #Assigne to 'data' the the function 'get' so that the variable 'data' now containes the matrix give by the user
+          s = solve(data,...) #Assigne to 's' the solution to the inverse of the matrix given in input
+          x$setinverse(s) #Call the function setinverse and store the invertse matrix
+          s #Print the inverted matrix
         }
